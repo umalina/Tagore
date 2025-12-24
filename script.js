242f6f886d613cea5e68a9db6e85b800400b9e7a
umalina <umalina991@gmail.com>
2025-12-24 21:13:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function openGallery(type){
   let imgs = [];
 
   if(type==="photo"){
-    imgs=["images/photo/p1.jpeg","images/photo/p2.jpeg","images/photo/p3.jpeg"];
+    imgs=["/Tagore/p1.jpeg","/Tagore/p2.jpeg","/Tagore/p3.jpeg"];
   }
   if(type==="video"){
     imgs=["images/video/v1.jpg","images/video/v2.jpg"];
@@ -74,5 +74,6 @@ galleryContent.addEventListener("mouseleave", ()=>{
     card.style.transform="";
   });
 });
+
 
 
